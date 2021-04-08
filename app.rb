6eace3c1e18925e1789aba8c2388725d8820252f
Pavel Skripka <ckripka24@rambler.ru>
2021-04-08 15:40:59 +0300

    Lame type Database

--- app.rb
+++ app.rb
@@ -2,6 +2,18 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+
+
+def db_init
+	@db = SQLite3::Database.new 'blog.db'
+	@db.results_as_hash = true
+end
+
+before do
+	db_init
+end
 
 get '/' do
 	erb "Hello!!! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -10,4 +22,10 @@ end
 
 get '/new' do
 	erb :new
+end
+
+post '/new' do
+	@post = params[:post]
+
+	erb "You tiped #{@post}"	
 end
