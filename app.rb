3f8ba460ad554a8bc1c42c84af54553c95a6997a
Pavel Skripka <ckripka24@rambler.ru>
2021-04-08 15:58:39 +0300

    Lame type to created table

--- app.rb
+++ app.rb
@@ -6,14 +6,24 @@ require 'sqlite3'
 
 
 
-def db_init
+def init_db
 	@db = SQLite3::Database.new 'blog.db'
 	@db.results_as_hash = true
 end
 
 before do
-	db_init
+	init_db
 end
+
+configure do
+	init_db
+	@db.execute 'CREATE TABLE IF NOT EXISTS Posts 
+		(
+			"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+			created_date DATE,
+			content TEXT
+			)'
+end 
 
 get '/' do
 	erb "Hello!!! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
